bef1f1cf6e0eda645d2c99bc5c342e514d110f74
Alasdair Nicol <alasdair@thenicols.net>
2017-01-03 22:42:22 +0000

    Contact method that sends email

--- app.py
+++ app.py
@@ -1,31 +1,56 @@
-from chalice import Chalice
+import urlparse
+
+import boto3
+
+from chalice import Chalice, BadRequestError
+from chalicelib import TO_EMAIL, FROM_EMAIL, SITE
 
 app = Chalice(app_name='contactform')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/contact', methods=['POST'], content_types=['application/x-www-form-urlencoded'], cors=True)
+def contact():
+    # Set these in chalice/__init__.py
+    to_email = TO_EMAIL
+    from_email = FROM_EMAIL
+    site = SITE
 
+    parsed = urlparse.parse_qs(app.current_request.raw_body)
 
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
+    # Default to [None] to avoid IndexError
+    name = parsed.get('name', [None])[0]
+    email = parsed.get('email', [None])[0]
+    message = parsed.get('message', [None])[0]
+
+    if not name:
+        raise BadRequestError("Please enter your name")
+    if not email:
+        raise BadRequestError("Please enter your email")
+    if not message:
+        raise BadRequestError("Please enter your message")
+
+    client = boto3.client('ses')
+
+    client.send_email(
+        Source=from_email,
+        Destination={
+            'ToAddresses': [to_email],
+        },
+        Message={
+            'Subject': {
+                'Data': "Message from " + name + " via " + site,
+            },
+            'Body': {'Text': {
+                'Data': message,
+            }},
+        },
+        # setting the reply-to header makes means that when you hit
+        # reply, the email goes to your visitor, not your from_email
+        ReplyToAddresses=[email],
+    )
+
+    response = {
+        "status": "OK",
+    }
+
+    return response
---
+++ chalicelib/__init__.py.tpl
@@ -0,0 +1,4 @@
+# Copy this file to __init__.py and update the settings
+TO_EMAIL = 'to@example.com'
+FROM_EMAIL = 'from@example.com'
+SITE = 'example.com'
